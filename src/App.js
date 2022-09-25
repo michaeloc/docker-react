2a80eca5101b9e04a5556c24a348697093660ec5
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2022-09-25 09:53:57 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Docker is nice! It's rock...I will get it. :)
         </p>
         <a
           className="App-link"
