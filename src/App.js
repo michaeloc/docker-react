152d314a4bc268160954b3d08186b652ec4005ba
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2022-09-25 15:22:58 -0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          Docker is nice! It's rock...I will get it. :). Now we teste a PR with feature branch.
+          Docker is nice! It's rock...I will get it. :). Now we teste a PR with feature branch. Teste again
         </p>
         <a
           className="App-link"
